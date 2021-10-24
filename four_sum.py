d9325211e73b76b8c3114fead48a5ef1a7d71a0c
Kalyankumar R <rkalyankumar@gmail.com>
2021-10-24 21:10:16 +0530

    four sum complete

--- four_sum.py
+++ four_sum.py
@@ -5,7 +5,34 @@ from typing import List
 
 class Solution:
 
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    def fourSum(self, nums, target):
+        def ksum(l, r, target, N, result, results):
+            print(result)
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:  # early termination
+                return
+            if N == 2: # two pointers solve sorted 2-sum problem
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        r -= 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else: # recursively reduce N
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i - 1] != nums[i]):
+                        ksum(i + 1, r, target - nums[i], N - 1, \
+                            result + [nums[i]], results)
+
+        nums.sort()
+        results = []
+        ksum(0, len(nums)-1, target, 4, [], results)
+        return results
 
 
 def main():
